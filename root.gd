80c3d5d4a3c9b78218d1a6aca2865b6605e4bd94
Randolph Cherrypepper <hi@randolphcherrypepper.stream>
2026-01-03 03:09:43 -0800

    levels can appear on screen

--- root.gd
+++ root.gd
@@ -1,4 +1,9 @@
+# nothing more classic than a large monofile.
+
 extends Node2D
+
+@export var BaseElements : Node2D
+@export var ViewPort : Node2D
 
 # modified from my code originally used in https://tallpear.itch.io/lil-guys-candy-run
 class RNG:
@@ -50,27 +55,122 @@ class Array2D:
 		return data[r][c]
 	func setv(r, c, value):
 		data[r][c] = value
-	func len():
-		return [len(data), len(data[0])]
+	func shape() -> Vector2:
+		return Vector2(len(data), len(data[0]))
+
+class CenteredArray2D extends Array2D:
+	var center
+	func _init(_n_rows: int, _n_cols: int, _default=0):
+		# treat n_rows and n_cols as being offset from (0,0) so (-n_rows,-n_cols) through (+n_rows, +n_cols)
+		center = Vector2(_n_rows, _n_cols)
+		_n_rows = _n_rows*2 + 1
+		_n_cols = _n_cols*2 + 1
+		super(_n_rows, _n_cols, _default)
+	func getv(r, c):
+		# row and column can be (-n_rows,-n_cols) through (+n_rows, +n_cols).
+		# adjust it to positive values only.
+		return super(r+center.x, c+center.y)
+	func setv(r, c, value):
+		super(r+center.x, c+center.y, value)
+	func rect() -> Rect2:
+		# return a shape considering it as a full rectangle
+		var parent_shape = shape()
+		print("parent ", parent_shape)
+		print("center ", center)
+		return Rect2(-center.x,-center.y,parent_shape.x,parent_shape.y)
+	func debug():
+		var dims = rect()
+		var values = ""
+		for r in range(dims.position.x, dims.position.x + dims.size.x):
+			for c in range(dims.position.y, dims.position.y + dims.size.y):
+				values += str(getv(r,c)) + " "
+			values += "\n"
+		print(values)
 
 class Level:
 	var rng
 	var level_number
-	var board: Array2D
-	# always leave "UNSET" at the start to indicate an initialization value
-	# always leave "LENGTH" at the end to get the valid length of the enum
-	enum m {UNSET, WALL, PATH, ROOM, STAIRS, LOOT, MOB, LENGTH}
+	var player_location: Vector2
+	var board: CenteredArray2D
+	# always leave "STAIRS" at the start.
+	# always leave "LAST" at the end to get the valid length of the enum
+	enum m {STAIRS, WALL, FLOOR, DOOR, LOOT, MOB, LAST}
 	func _init(_rng, _level):
 		rng = _rng
-		level_number = _level
-		board = Array2D.new(3**level_number, 3**level_number)
+		# level alone makes the first level a bit too bleak.
+		level_number = _level #+ 1 # TODO commented out for DEBUG
+		board = CenteredArray2D.new(3*level_number, 3*level_number)
+		# for now, fill in the board with random values.
+		var dims: Rect2 = board.rect()
+		for r in range(dims.position.x, dims.position.x + dims.size.x):
+			for c in range(dims.position.y, dims.position.y + dims.size.y):
+				# TODO replace temporary fillin
+				board.setv(r,c,randi_range(m.STAIRS+1, m.LAST-1))
+		board.debug()
+	func set_player_location(loc: Vector2):
+		player_location = loc
+
+class Renderer:
+	var be_cr: ColorRect
+	var be_dtfs: RichTextLabel
+	var be_commands: RichTextLabel
+	var be_help: RichTextLabel
+	var vp_container: Node2D
+	var ts: Vector2
+	var charmap: Dictionary
+	
+	func _init(_be_container, _vp_container, _ts):
+		be_cr = _be_container.get_node_and_resource("ColorRect")[0]
+		be_dtfs = _be_container.get_node_and_resource("DebugTestFontSize")[0]
+		be_commands = _be_container.get_node_and_resource("Commands")[0]
+		be_help = _be_container.get_node_and_resource("Help")[0]
+		vp_container = _vp_container
+		ts = _ts
+		
+		# setup dictionary of displays.
+		charmap = {
+			#enum m {STAIRS, WALL, FLOOR, DOOR, LOOT, MOB, LAST}
+			Level.m.STAIRS: '[color="#333333"]>[/color]',
+			Level.m.WALL: '[color="#666666"]#[/color]',
+			Level.m.FLOOR: '[color="#999999"].[/color]',
+			Level.m.DOOR: '[color="#cccc00"]#[/color]',
+			Level.m.LOOT: '[color="#00cc00"]?[/color]',
+			Level.m.MOB: '[color="#cc0000"]![/color]',
+		}
+	
+	func base_elements_default():
+		be_cr.show()
+		be_dtfs.hide()
+		be_commands.show()
+		be_help.hide()
+
+	func show_viewport(level: Level):
+		base_elements_default()
+		var center = level.player_location
+		var board = level.board
+		var viewport: RichTextLabel = vp_container.get_children()[0]
+		var map = "\n\n"
+		var dims: Rect2 = board.rect()
+		print(dims)
+		for r in range(dims.position.x, dims.position.x + dims.size.x):
+			for c in range(dims.position.y, dims.position.y + dims.size.y):
+				#print(r, c, center)
+				if r == center.x and c == center.y:
+					map += '[color="#00cc00"]@[/color]'
+				else:
+					map += charmap[board.getv(r,c)]
+			map += "\n"
+		viewport.text = map
 
 var my_rng
 var tick
-
+var renderer
 func _ready():
+	renderer = Renderer.new(BaseElements, ViewPort, Vector2(25, 80))
 	# TODO add seed from user here
 	my_rng = RNG.new(0)
 	tick = 0
 	var curr_level = 1
 	var level = Level.new(my_rng, curr_level)
+	level.set_player_location(Vector2(0,0))
+	renderer.show_viewport(level)
