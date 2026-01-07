a31a0fff2c12e0b566b00e63583fb7ed2cc2b322
Randolph Cherrypepper <hi@randolphcherrypepper.stream>
2026-01-07 00:38:22 -0800

    path and room gen mostly running

--- root.gd
+++ root.gd
@@ -5,23 +5,25 @@ extends Node2D
 @export var BaseElements : Node2D
 @export var ViewPort : Node2D
 
+signal kill_me_now_plz
+
 # modified from my code originally used in https://tallpear.itch.io/lil-guys-candy-run
 class RNG:
 	var game_seed
-	enum rtype {MAP, ITEM, NPC}
+	enum rtype {PATH, ROOM, ITEM, NPC}
 	func _init(_seed):
 		game_seed = _seed
-	func get_value(what: rtype, where: Vector2, level: int) -> int:
+	func get_value(what: rtype, where: Vector2, level: int, additional: String = "") -> int:
 		# too much granularity means moving an asset a few pixels in the editor will change
 		# the results of procedural generation.
 		# drop decimals off x,y location by converting to int.
 		# divide to further reduce granularity, because pixel locations are after scaling.
-		var int_location = Vector2i(where/8.)
+		var int_location = where #Vector2i(where/8.)
 		# hashing context will do some math for us.
 		var ctx = HashingContext.new()
 		# combine all the parts into a string as
 		# seed|level(x,y)typenumber
-		var input = str(game_seed) + "|" + str(level) + str(int_location) + str(what)
+		var input = str(game_seed) + "|" + str(level) + str(int_location) + str(what) + additional
 		# md5 is the fastest of the options. generates a 128 bit long string/number.
 		ctx.start(HashingContext.HASH_MD5)
 		# convert string into UTF8 bytes and add to the hashing context
@@ -85,33 +87,217 @@ class CenteredArray2D extends Array2D:
 			values += "\n"
 		print(values)
 
+const directions = {
+	1: Vector2(1,0),
+	2: Vector2(-1,0),
+	4: Vector2(0,1),
+	8: Vector2(0,-1),
+}
+
 class Level:
 	var rng
 	var level_number
 	var renderer
 	var player_location: Vector2
 	var board: CenteredArray2D
+	var kill_me_now_plz
 	# always leave "STAIRS" at the start.
 	# always leave "LAST" at the end to get the valid length of the enum
 	enum m {STAIRS, WALL, FLOOR, DOOR, LOOT, MOB, LAST}
-	func _init(_rng, _level, _renderer):
+	func _init(_kill_me_now_plz, _rng, _level, _renderer, start_location: Vector2):
+		player_location = start_location
 		rng = _rng
 		renderer = _renderer
+		kill_me_now_plz = _kill_me_now_plz
 		# level alone makes the first level a bit too bleak.
 		level_number = _level + 1
-		board = CenteredArray2D.new(3*level_number, 3*level_number)
-		# for now, fill in the board with random values.
-		var dims: Rect2 = board.rect()
-		for r in range(dims.position.x, dims.position.x + dims.size.x):
-			for c in range(dims.position.y, dims.position.y + dims.size.y):
-				# TODO replace temporary fillin
-				board.setv(r,c,randi_range(m.STAIRS+1, m.LAST-1))
-		#board.debug()
-	func set_player_location(loc: Vector2):
-		player_location = loc
+		board = CenteredArray2D.new(3*level_number, 3*level_number, m.WALL)
+		var bounds: Rect2 = board.rect()
+		if false: # TODO remove this DEBUG random map filler
+			# for now, fill in the board with random values.
+			for r in range(bounds.position.x, bounds.position.x + bounds.size.x):
+				for c in range(bounds.position.y, bounds.position.y + bounds.size.y):
+					board.setv(r,c,randi_range(m.STAIRS+1, m.LAST-1))
+			board.debug()
+		# Perform a random walk from the starting location.
+		recursive_build_path_dfs(0, start_location, true)
+		recursive_build_rooms(start_location)
+
+	func set_region(region: Rect2, m_type: m, interior=true):
+		var x_start = region.position.x
+		var x_stop = region.position.x+region.size.x
+		if (x_stop-x_start) < 0:
+			# the two values are not in increasing order. fix that.
+			var x_tmp = x_start
+			x_start = x_stop
+			x_stop = x_tmp
+		x_stop -= 1
+		var y_start = region.position.y
+		var y_stop = region.position.y+region.size.y
+		if (y_stop-y_start) < 0:
+			var y_tmp = y_start
+			y_start = y_stop
+			y_stop = y_tmp
+		y_stop -= 1
+		if interior:
+			for x in range(x_start, x_stop+1):
+				for y in range(y_start, y_stop+1):
+					board.setv(x, y, m_type)
+		if not interior:
+			for x in range(x_start, x_stop+1):
+				for y in [y_start, y_stop]:
+					board.setv(x, y, m_type)
+			for y in range(y_start, y_stop+1):
+				for x in [x_start, x_stop]:
+					board.setv(x, y, m_type)
+
+	func build_doors(region: Rect2):
+		var bounds: Rect2 = board.rect()
+		var x_start = region.position.x
+		var x_stop = region.position.x+region.size.x
+		if (x_stop-x_start) < 0:
+			# the two values are not in increasing order. fix that.
+			var x_tmp = x_start
+			x_start = x_stop
+			x_stop = x_tmp
+		x_stop -= 1
+		var y_start = region.position.y
+		var y_stop = region.position.y+region.size.y
+		if (y_stop-y_start) < 0:
+			var y_tmp = y_start
+			y_start = y_stop
+			y_stop = y_tmp
+		y_stop -= 1
+		for test_set in [
+			[Vector2(-1,0), Vector2(1,0)],
+			[Vector2(0,-1), Vector2(0,1)]
+		]:
+			# TODO if path left/right, check for doors up/down and do not make another door if found
+			for x in range(x_start, x_stop+1):
+				for y in [y_start, y_stop]:
+					var paths = 0
+					for check_direction in test_set:
+						var check_point = Vector2(x,y) + check_direction
+						if bounds.has_point(check_point):
+							if board.getv(check_point.x, check_point.y) in [m.FLOOR, m.STAIRS]:
+								# a diagonal is not a wall, don't add a path here
+								paths += 1
+						if paths == 2:
+							board.setv(x, y, m.DOOR)
+			for y in range(y_start, y_stop+1):
+				for x in [x_start, x_stop]:
+					var paths = 0
+					for check_direction in test_set:
+						var check_point = Vector2(x,y) + check_direction
+						if bounds.has_point(check_point):
+							if board.getv(check_point.x, check_point.y) in [m.FLOOR, m.STAIRS]:
+								# a diagonal is not a wall, don't add a path here
+								paths += 1
+						if paths == 2:
+							board.setv(x, y, m.DOOR)
+
+	func recursive_build_path_dfs(depth, location: Vector2, starting=false) -> bool:
+		# shrink the board so that paths never lead off the edge
+		var bounds: Rect2 = board.rect().grow_individual(-1, -1, -1, -1)
+		# prevent too much nesting
+		if depth == 32:
+			return false
+		# make sure this will not create a 2x2 region by testing each set of 3 points
+		for test_set in [
+			[Vector2(-1,0), Vector2(-1,-1), Vector2(0,-1)],
+			[Vector2(1,0), Vector2(1,1), Vector2(0,1)],
+			[Vector2(1,0), Vector2(1,-1), Vector2(0,-1)],
+			[Vector2(-1,0), Vector2(-1,1), Vector2(0,1)],
+		]:
+			var test_points = 0
+			for check_direction in test_set:
+				var check_point = location + check_direction
+				if bounds.has_point(check_point):
+					if board.getv(check_point.x, check_point.y) != m.WALL:
+						# a diagonal is not a wall, don't add a path here
+						test_points += 1
+			if test_points == 3:
+				# this point does not pass the diagonals check. too many diagonals.
+				return false
+		# this point passes checks, mark it.
+		if starting:
+			board.setv(location.x, location.y, m.STAIRS)
+		else:
+			board.setv(location.x, location.y, m.FLOOR)
+		# get a random value determinstically
+		var rngv: int = rng.get_value(rng.rtype.PATH, location, level_number)
+		# choose one or more directions from up to 4 choices
+		var direction = rngv % 16
+		while direction == 0: # && starting:
+			if rngv == 0:
+				# could not generate a valid path. GIVE UP OMG DO NOT INFINITE LOOP PLZ
+				kill_me_now_plz.emit()
+			# remove the bits used above; avoids too many slow RNG calls
+			@warning_ignore("integer_division")
+			rngv = rngv / 16
+			direction = rngv % 16
+		for val_check in directions.keys():
+			if (direction & val_check):
+				var new_location = location + directions[val_check]
+				if bounds.has_point(new_location):
+					# avoid touching a location that already has a path. 
+					if board.getv(new_location.x, new_location.y) == m.WALL:
+						recursive_build_path_dfs(depth+1, new_location)
+		return true
+
+	func recursive_build_rooms(starting_location: Vector2):
+		# shrink the board so that paths never lead off the edge
+		var bounds: Rect2 = board.rect().grow_individual(-1, -1, -1, -1)
+		var shape = board.shape()
+		# get a random value determinstically
+		var rngv: int = rng.get_value(rng.rtype.ROOM, starting_location, level_number)
+		# decide how many rooms to attempt in this level.
+		var n_rooms = rngv % (2*level_number)
+		if n_rooms < 2:
+			n_rooms = 2
+		var tot_rooms = 0
+		var attempts = 0
+		print("n_rooms ", n_rooms)
+		while tot_rooms < n_rooms and attempts < 100:
+			var room_rngv: int = rng.get_value(rng.rtype.ROOM, starting_location, level_number, ":" + str(attempts))
+			attempts += 1
+			# pull a fourth of the 32 bits for width
+			var width = (room_rngv % 16384) % int(bounds.size.x/3.)
+			if width < 5:
+				width = 5
+			# don't remove the full amount of bits used, but offset them somewhat
+			@warning_ignore("integer_division")
+			room_rngv = room_rngv / 256
+			var height = (room_rngv % 16384) % int(bounds.size.y/3.)
+			if height < 5:
+				height = 5
+			@warning_ignore("integer_division")
+			room_rngv = room_rngv / 256
+			var position_x = room_rngv % int(shape.x - width)
+			@warning_ignore("integer_division")
+			room_rngv = room_rngv / 256
+			var position_y = room_rngv % int(shape.y - height)
+			@warning_ignore("integer_division")
+			room_rngv = room_rngv / 256
+			var potential_room = Rect2(position_x + bounds.position.x, position_y + bounds.position.y, width, height)
+			if not bounds.encloses(potential_room):
+				# room is not in the map.
+				continue
+			if potential_room.has_point(starting_location):
+				# don't cover the starting location with a room. always hallway.
+				continue
+			# TODO test that there is a path on the boundary of the room.
+			# valid room!
+			tot_rooms += 1
+			set_region(potential_room.grow_individual(-1,-1,-1,-1), m.FLOOR)
+			set_region(potential_room, m.WALL, false)
+			build_doors(potential_room)
+		print("tot_rooms ", tot_rooms, " with ", attempts)
+
 	func move_player(change: Vector2):
 		player_location += change
 		draw(false)
+
 	func draw(swap=false):
 		renderer.fill_viewport(self, swap)
 		if swap:
@@ -141,24 +327,24 @@ class Renderer:
 		# setup dictionary of displays.
 		charmap = {
 			0: {
-				Level.m.STAIRS: '[color="#333333"]>[/color]',
-				Level.m.WALL: '[color="#666666"]#[/color]',
-				Level.m.FLOOR: '[color="#999999"].[/color]',
-				Level.m.DOOR: '[color="#cccc00"]#[/color]',
-				Level.m.LOOT: '[color="#00cc00"]?[/color]',
+				Level.m.STAIRS: '[color="#b0b080"]>[/color]',
+				Level.m.WALL: '[color="#c0a060"]#[/color]',
+				Level.m.FLOOR: '[color="#00cc00"].[/color]',
+				Level.m.DOOR: '[color="#cccc00"]+[/color]',
+				Level.m.LOOT: '[color="#999999"]?[/color]',
 				Level.m.MOB: '[color="#cc0000"]![/color]',
 				Level.m.LAST: ' ', # acts as empty as well
-				'player': '[color="#00cc00"]@[/color]'
+				'player': '[color="#cccc00"][outline_size=1][outline_color="#ffffff"]@[/outline_color][/outline_size][/color]'
 			},
 			1: {
-				Level.m.STAIRS: '[color="#333333"]>[/color]',
-				Level.m.WALL: '[color="#666666"]#[/color]',
-				Level.m.FLOOR: '[color="#999999"].[/color]',
-				Level.m.DOOR: '[color="#cccc00"]#[/color]',
-				Level.m.LOOT: '[color="#00cc00"]?[/color]',
+				Level.m.STAIRS: '[color="#b0b080"]>[/color]',
+				Level.m.WALL: '[color="#c0a060"]#[/color]',
+				Level.m.FLOOR: '[color="#00cc00"].[/color]',
+				Level.m.DOOR: '[color="#cccc00"]+[/color]',
+				Level.m.LOOT: '[color="#999999"]?[/color]',
 				Level.m.MOB: '[color="#cccc00"]![/color]',
 				Level.m.LAST: ' ', # acts as empty as well
-				'player': '[color="#00cc00"]@[/color]'
+				'player': '[color="#cccc00"][outline_size=1][outline_color="#ffffff"]@[/outline_color][/outline_size][/color]'
 			},
 		}
 	
@@ -230,13 +416,14 @@ var level
 var last_render = 0
 const animation_rate = 0.7
 func _ready():
+	kill_me_now_plz.connect(quit)
 	renderer = Renderer.new(BaseElements, ViewPort, Vector2(25, 80), Vector2(11,40))
 	# TODO add seed from user here
-	my_rng = RNG.new(0)
+	my_rng = RNG.new(4)
 	tick = 0
-	var curr_level = 1
-	level = Level.new(my_rng, curr_level, renderer)
-	level.set_player_location(Vector2(0,0))
+	var curr_level = 5
+	var starting_location = Vector2(0,0)
+	level = Level.new(kill_me_now_plz, my_rng, curr_level, renderer, starting_location)
 	level.draw(false)
 
 func _process(delta):
@@ -254,7 +441,7 @@ func quit():
 func _notification(what):
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
 		# user is trying to close the window. quit the game.
-		quit()
+		kill_me_now_plz.emit()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
@@ -266,4 +453,4 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_right"):
 		level.move_player(Vector2(0,1))
 	if event.is_action_pressed("ui_quit"):
-		quit()
+		kill_me_now_plz.emit()
